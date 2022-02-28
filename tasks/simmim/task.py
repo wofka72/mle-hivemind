6835bbb3e0d892c2e9c776630f93915ffa7697d9
justheuristic <justheuristic@gmail.com>
2022-02-28 07:11:29 +0300

    collator

--- tasks/simmim/task.py
+++ tasks/simmim/task.py
@@ -25,6 +25,12 @@ hivemind.use_hivemind_log_handler("in_root_logger")
 logger = hivemind.get_logger()
 
 
+FEATURE_EXTRACTOR_TYPES = {
+    conf.model_type: feature_extractor_class
+    for conf, feature_extractor_class in FEATURE_EXTRACTOR_MAPPING.items()
+}
+
+
 @register_task("mim")
 class MaskedImageModelingTask(TrainingTaskBase):
     """A container that defines the training config, model, tokenizer, optimizer and other local training utilities"""
@@ -35,12 +41,13 @@ class MaskedImageModelingTask(TrainingTaskBase):
         self, peer_args: BasePeerArguments, trainer_args: HFTrainerArguments, collab_args: CollaborativeArguments
     ):
         transformers.set_seed(trainer_args.seed)  # seed used for initialization
+        self.feature_extractor = FEATURE_EXTRACTOR_TYPES[self.config.model_type]()
         self.dataset_path = peer_args.dataset_path
         self.config = SwinConfig(
             image_size=192,
             patch_size=4,
             embed_dim=512,
-            depths=[2, 2, 26, 2],
+            depths=[2, 2, 2, 2],
             num_heads=[16, 32, 64, 128],
             window_size=6,
         )
@@ -105,18 +112,12 @@ class MaskedImageModelingTask(TrainingTaskBase):
     @property
     def training_dataset(self):
         if self._training_dataset is None:
-            FEATURE_EXTRACTOR_TYPES = {
-                conf.model_type: feature_extractor_class
-                for conf, feature_extractor_class in FEATURE_EXTRACTOR_MAPPING.items()
-            }
-            feature_extractor = FEATURE_EXTRACTOR_TYPES[self.config.model_type]()
-
             self._training_dataset = SimMIMDataset(
                 self.dataset_path,
                 image_size=self.config.image_size,
                 model_patch_size=self.config.patch_size,
                 mask_patch_size=32, mask_ratio=0.5,
-                feature_extractor=feature_extractor
+                feature_extractor=self.feature_extractor
             )
         return self._training_dataset
 
